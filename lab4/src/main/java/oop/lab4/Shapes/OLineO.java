5966ce123d457d7f96e50cd2ce77f1c8e8ef1f32
AndriiHalai <a.halai.im21@kpi.ua>
2023-11-12 20:44:32 +0200

    Add OLineO and cube classes

---
+++ lab4/src/main/java/oop/lab4/Shapes/OLineO.java
@@ -0,0 +1,25 @@
+package oop.lab4.Shapes;
+
+import javafx.scene.canvas.Canvas;
+
+public class OLineO extends Shape {
+    @Override
+    public void show(Canvas canvas) {
+
+    }
+
+    @Override
+    public void onMousePressed(Canvas canvas) {
+
+    }
+
+    @Override
+    public void onMouseDragged(Canvas canvas) {
+
+    }
+
+    @Override
+    public void onMouseReleased(Canvas canvas) {
+
+    }
+}
