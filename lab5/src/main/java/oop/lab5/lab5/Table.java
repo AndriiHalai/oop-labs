114006c1e9b1c8d5a5b8beabbc92321e82e000ed
AndriiHalai <a.halai.im21@kpi.ua>
2023-12-04 23:08:21 +0200

    Implement add to table function

--- lab5/src/main/java/oop/lab5/lab5/Table.java
+++ lab5/src/main/java/oop/lab5/lab5/Table.java
@@ -1,29 +1,29 @@
 package oop.lab5.lab5;
 
+import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import oop.lab5.lab5.Shapes.Shape;
 
 import java.io.IOException;
 
 public class Table {
-    private Table() {}
+    public ObservableList<Shape> shapes;
 
-    private static class TableHolder {
-        static final Table INSTANCE = new Table();
+    public Stage stage;
+
+    public Table() throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(Table.class.getResource("table.fxml"));
+        Parent root = fxmlLoader.load();
+        this.stage = new Stage();
+        stage.setTitle("Table");
+        stage.setScene(new Scene(root));
+        TableController.setController(fxmlLoader.getController());
     }
 
     public void show() throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("table.fxml"));
-        Parent root = fxmlLoader.load();
-        Stage window = new Stage();
-        window.setTitle("Table");
-        window.setScene(new Scene(root));
-        window.show();
-    }
-
-    public static Table getInstance() {
-        return TableHolder.INSTANCE;
+        stage.show();
     }
 }
--- lab5/src/main/java/oop/lab5/lab5/TableController.java
+++ lab5/src/main/java/oop/lab5/lab5/TableController.java
@@ -1,2 +1,67 @@
-package oop.lab5.lab5;public class TableController {
+package oop.lab5.lab5;
+
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import oop.lab5.lab5.Shapes.Shape;
+import oop.lab5.lab5.Shapes.Test;
+
+public class TableController {
+
+    public ObservableList<Shape> shapes;
+
+    public static TableController controller;
+
+    @FXML
+    public TableView<Shape> tableView;
+
+    @FXML
+    private AnchorPane root;
+
+    @FXML
+    private TableColumn<Shape, String> nameCol;
+
+    @FXML
+    private TableColumn<Shape, Integer> x1Col;
+
+    @FXML
+    private TableColumn<Shape, Integer> y1Col;
+
+    @FXML
+    private TableColumn<Shape, Integer> x2Col;
+
+    @FXML
+    private TableColumn<Shape, Integer> y2Col;
+
+    @FXML
+    public void initialize() {
+        nameCol.setCellValueFactory(new PropertyValueFactory<>("name"));
+        x1Col.setCellValueFactory(new PropertyValueFactory<>("x1"));
+        y1Col.setCellValueFactory(new PropertyValueFactory<>("y1"));
+        x2Col.setCellValueFactory(new PropertyValueFactory<>("x2"));
+        y2Col.setCellValueFactory(new PropertyValueFactory<>("y2"));
+    }
+
+    public void show() {
+        Stage window = new Stage();
+        Scene scene = new Scene(root, 700, 400);
+        Test test = new Test("Test", 1,1,1,1);
+        tableView.getItems().add(test);
+
+        window.setScene(scene);
+        window.show();
+    }
+
+    public void add(Shape shape) {
+        tableView.getItems().add(shape);
+    }
+
+    public static void setController(TableController val) {
+        controller = val;
+    }
 }
