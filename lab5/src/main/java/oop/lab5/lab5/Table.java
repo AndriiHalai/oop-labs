9354b3b1137df0b91868793f78b555ccd7b2270e
AndriiHalai <a.halai.im21@kpi.ua>
2023-12-06 15:06:34 +0200

    Implement singleton pattern for table

--- lab5/src/main/java/oop/lab5/lab5/Table.java
+++ lab5/src/main/java/oop/lab5/lab5/Table.java
@@ -4,6 +4,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import oop.lab5.lab5.Shapes.Shape;
 
 import java.io.IOException;
 
@@ -13,7 +14,7 @@ public class Table {
 
     private final TableController controller;
 
-    public Table() throws IOException {
+    private Table() throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Table.class.getResource("table.fxml"));
         Parent root = fxmlLoader.load();
         this.stage = new Stage();
@@ -23,8 +24,28 @@ public class Table {
         TableController.setController(fxmlLoader.getController());
     }
 
+    private static class TableHolder {
+        public static final Table INSTANCE;
+
+        static {
+            try {
+                INSTANCE = new Table();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    public static Table getInstance() {
+        return TableHolder.INSTANCE;
+    }
+
     public void show() throws IOException {
         stage.show();
+    }
+
+    public void addToTable(Shape shape) {
+        controller.add(shape);
     }
 
     public TableController getController() {
