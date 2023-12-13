4a87cdf9e87bdd0fa197f6d8ae7b24ffc832de6d
AndriiHalai <a.halai.im21@kpi.ua>
2023-12-13 22:36:07 +0200

    Update writing to a txt file format

--- lab5/src/main/java/oop/lab5/lab5/TableController.java
+++ lab5/src/main/java/oop/lab5/lab5/TableController.java
@@ -52,12 +52,13 @@ public class TableController {
     public void saveToFile() {
         try {
            FileWriter fw = new FileWriter("D:/homework/2nd-year/oop/oop-labs/lab5/src/main/java/oop/lab5/lab5/shapes.txt");
+           fw.write("Shape_Name \t" + "x1\t" + "x2\t" + "x3\t" + "x4\t\n");
            for (Shape shape : SHAPES_LIST) {
-               fw.write(shape.getName() + "|"
-                       + shape.getX1() + "|"
-                       + shape.getY1() + "|"
-                       + shape.getX2() + "|"
-                       + shape.getY2());
+               fw.write(shape.getName() + "\t"
+                       + Math.round(shape.getX1()) + "\t"
+                       + Math.round(shape.getY1()) + "\t"
+                       + Math.round(shape.getX2()) + "\t"
+                       + Math.round(shape.getY2()) );
                fw.write("\n");
            }
            fw.close();
