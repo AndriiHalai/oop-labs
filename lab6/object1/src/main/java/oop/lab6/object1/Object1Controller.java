787114876283e448d613eb3936731b6bd89c6e27
AndriiHalai <a.halai.im21@kpi.ua>
2023-12-25 11:30:04 +0200

    Add openning another jar file logic

--- lab6/object1/src/main/java/oop/lab6/object1/Object1Controller.java
+++ lab6/object1/src/main/java/oop/lab6/object1/Object1Controller.java
@@ -1,8 +1,26 @@
 package oop.lab6.object1;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+
+import java.io.IOException;
 
 public class Object1Controller {
+    @FXML
+    private Button startButton;
+
+    @FXML
+    public void initialize() {
+        startButton.setOnAction(actionEvent -> {
+            String path = "D:\\homework\\2nd-year\\oop\\oop-labs\\lab6\\object2\\out\\artifacts\\object2_jar\\object2.jar";
+            String[] command = {"java", "-jar", path};
+            ProcessBuilder processBuilder = new ProcessBuilder(command);
+            try {
+                Process p = processBuilder.start();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
 
 }
